8770c264dab8c58103c0681010244e7805d5011f
Hossein Mayboudi <smhmayboudi@gmail.com>
2021-11-25 03:20:02 +0330

    feat: seventh commit

--- script/perf.sh
+++ script/perf.sh
@@ -9,7 +9,7 @@ set -o nounset
 rm -fr /tmp/pgo-data
 
 # STEP 1: Build the instrumented binaries
-RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" \
+RUSTFLAGS=`-Cprofile-generate=/tmp/pgo-data` \
     cargo build --package fip_api --release --target x86_64-apple-darwin
 
 # STEP 2: Run the instrumented binaries with some typical data
@@ -17,9 +17,9 @@ RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" \
 ../target/x86_64-apple-darwin/release/myprogram-mydata2.csv
 ../target/x86_64-apple-darwin/release/myprogram-mydata3.csv
 
-# STEP 3: Merge the ".profraw" files into a ".profdata" file
+# STEP 3: Merge the `.profraw` files into a `.profdata` file
 llvm-profdata merge --output=/tmp/pgo-data/merged.profdata /tmp/pgo-data
 
-# STEP 4: Use the ".profdata" file for guiding optimizations
-RUSTFLAGS="-Cllvm-args=-pgo-warn-missing-function -Cprofile-use=/tmp/pgo-data/merged.profdata" \
+# STEP 4: Use the `.profdata` file for guiding optimizations
+RUSTFLAGS=`-Cllvm-args=-pgo-warn-missing-function -Cprofile-use=/tmp/pgo-data/merged.profdata` \
     cargo build --package fip_api --release --target x86_64-apple-darwins
--- script/tag.sh
+++ script/tag.sh
@@ -5,8 +5,8 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-COMMIT_SHA=${1:-""}
-VERSION=${2:-"0.1.0"}
+VERSION=${1:-"0.1.0"}
+COMMIT_SHA=${2:-""}
 
 GIT_COMMITTER_DATE=$(git log --format=%aD --max-count=1 ${COMMIT_SHA}) git tag -a -m "version ${VERSION}" "v${VERSION}"
 git push --tags
