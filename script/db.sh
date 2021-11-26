87597b4ada8bc99832563f4a5734835a9db1e15f
Hossein Mayboudi <smhmayboudi@gmail.com>
2021-11-26 17:55:01 +0330

    build: update scripts

--- script/db.sh
+++ script/db.sh
@@ -5,20 +5,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-cd ../fip_at
-rm -fr at.db*
-sqlx database create --database-url sqlite:at.db
-sqlx migrate run --database-url sqlite:at.db
-cd ../fip_jwks
-rm -fr jwks.db*
-sqlx database create --database-url sqlite:jwks.db
-sqlx migrate run  --database-url sqlite:jwks.db
-cd ../fip_rt
-rm -fr rt.db*
-sqlx database create --database-url sqlite:rt.db
-sqlx migrate run --database-url sqlite:rt.db
-cd ../fip_user
-rm -fr user.db*
-sqlx database create --database-url sqlite:user.db
-sqlx migrate run --database-url sqlite:user.db
-cd ..
+PACKAGES="api at jwks rt user"
+
+for PACKAGE in ${PACKAGES}; do
+    cd ./fip_${PACKAGE }
+    rm -fr ${PACKAGE }.db*
+    sqlx database create --database-url sqlite:${PACKAGE }.db
+    sqlx migrate run --database-url sqlite:${PACKAGE }.db
+    cd ..
+done
