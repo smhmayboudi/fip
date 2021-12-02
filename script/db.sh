9f901042b1eb6d575fbd4079e1ec250bce9c855a
Hossein Mayboudi <smhmayboudi@gmail.com>
2021-12-02 16:24:50 +0330

    build: update code

--- script/db.sh
+++ script/db.sh
@@ -8,9 +8,9 @@ set -o nounset
 PACKAGES="api at jwks rt user"
 
 for PACKAGE in ${PACKAGES}; do
-    cd ./fip_${PACKAGE}
-    rm -fr ${PACKAGE}.db*
-    sqlx database create --database-url sqlite:${PACKAGE}.db
-    sqlx migrate run --database-url sqlite:${PACKAGE}.db
-    cd ..
+    cd "./fip_${PACKAGE}"
+    rm -fr "${PACKAGE}.db*"
+    sqlx database create --database-url "sqlite:${PACKAGE}.db"
+    sqlx migrate run --database-url "sqlite:${PACKAGE}.db"
+    cd -
 done
