b6a9714f3cf751e851661ec12df49d27879d2018
Hossein Mayboudi <smhmayboudi@gmail.com>
2021-12-31 10:24:15 +0330

    build: update configs

--- script/tag.sh
+++ script/tag.sh
@@ -8,5 +8,5 @@ set -o nounset
 VERSION=${1:-"0.1.0"}
 COMMIT_SHA=${2:-""}
 
-GIT_COMMITTER_DATE=$(git log --format="%aD" --max-count=1 "${COMMIT_SHA}") git tag -a -m "version ${VERSION}" "v${VERSION}" "${COMMIT_SHA}"
+GIT_COMMITTER_DATE=$(git --no-pager log --format="%aD" --max-count=1 "${COMMIT_SHA}") git tag -a -m "version ${VERSION}" "v${VERSION}" "${COMMIT_SHA}"
 git push --tags
